0104288e65280097c476f922c66daab1e6c656a4
jeremypark <jkp5kj@virginia.edu>
2014-04-01 10:44:32 -0400

    splash screen activity

---
+++ Test/gen/edu/virginia/cs2110/jkp5kj/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.virginia.cs2110.jkp5kj.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Test/src/edu/virginia/cs2110/jkp5kj/test/TestActivity.java
@@ -0,0 +1,78 @@
+package edu.virginia.cs2110.jkp5kj.test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TestActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
+		Thread thread = new Thread() {
+			
+			@Override
+			public void run() {
+				try {
+					sleep(1000);
+					startActivity(new Intent(getApplicationContext(), MainActivity.class));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		};
+		
+		thread.start();
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.test, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_test, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
